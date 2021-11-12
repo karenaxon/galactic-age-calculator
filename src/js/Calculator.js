fc303fc7d5fd2d72fbfccd5f9ad0e3a55c64f3b5
Kaxon <karenaxon@users.noreply.github.com>
2021-11-12 09:31:48 -0800

    Add Calculator test 2 Green phase.

--- src/js/Calculator.js
+++ src/js/Calculator.js
@@ -1,9 +1,15 @@
 export default class Calculator{
   constructor(humanAge){
     this.humanAge = humanAge;
-    this.mercuryAge = "";
+    this.mercuryAge = this.mercuryAgeCalculate();
     this.venusAge = "";
     this.marsAge = "";
     this.jupiterAge = "";
   }
+
+  mercuryAgeCalculate() {
+    let mercuryAge = this.humanAge *.24;
+    console.log(this.mercuryAge);
+    return mercuryAge;
+  }  
 }
